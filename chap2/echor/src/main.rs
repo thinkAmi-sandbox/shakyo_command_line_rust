8c08b820afa17e801cf460f37b408cd67616f9c8
thinkAmi <dev.thinkami+git@gmail.com>
2025-01-03 21:10:08 +0900

    2章のechoコマンドを完成

--- chap2/echor/src/main.rs
+++ chap2/echor/src/main.rs
@@ -20,5 +20,13 @@ fn main() {
         )
         .get_matches();
 
-    println!("{:#?}", matches);
+    // 動作確認のため追加していたので、コメントアウト
+    // println!("{:#?}", matches);
+
+    let text = matches.values_of_lossy("text").unwrap();
+
+    let omit_newline = matches.is_present("omit_newline");
+    let ending = if omit_newline { "" } else { "\n" };
+
+    print!("{}{}", text.join(" "), ending);
 }
