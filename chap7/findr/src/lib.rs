b9bdb086b3aa4f5b678732e29e8a162e04316526
thinkAmi <dev.thinkami+git@gmail.com>
2025-01-08 22:11:36 +0900

    7章のリファクタリング後の実装

--- chap7/findr/src/lib.rs
+++ chap7/findr/src/lib.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use clap::{App, Arg};
 use regex::Regex;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 use crate::EntryType::{Dir, File, Link};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -88,26 +88,61 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    for path in config.paths {
-        for entry in WalkDir::new(path) {
-            match entry {
-                Err(e) => eprintln!("{}", e),
-                Ok(entry) => {
-                    if (config.entry_types.is_empty() || config.entry_types.iter().any(|t| {
-                        match t {
-                            Link => entry.file_type().is_symlink(),
-                            Dir => entry.file_type().is_dir(),
-                            File => entry.file_type().is_file(),
-                        }
-                    })) && (config.names.is_empty() || config.names.iter().any(|re| {
-                        re.is_match(&entry.file_name().to_string_lossy())
-                    }))
-                    {
-                        println!("{}", entry.path().display());
-                    }
-                }
+    let type_filter = |entry: &DirEntry| {
+        config.entry_types.is_empty() || config.entry_types.iter().any(|t| {
+            match t {
+                Link => entry.file_type().is_symlink(),
+                Dir => entry.file_type().is_dir(),
+                File => entry.file_type().is_file(),
             }
-        }
+        })
+    };
+
+    let name_filter = |entry: &DirEntry| {
+        config.names.is_empty() || config.names.iter().any(|re|{
+            re.is_match(&entry.file_name().to_string_lossy())
+        })
+    };
+
+    for path in &config.paths {
+        let entries = WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| match e {
+                Err(e) => {
+                    eprintln!("{}", e);
+                    None
+                }
+                Ok(entry) => Some(entry)
+            })
+            .filter(type_filter)
+            .filter(name_filter)
+            .map(|e| e.path().display().to_string())
+            .collect::<Vec<_>>();
+
+        println!("{}", entries.join("\n"))
     }
+
+    // リファクタリング前
+    // for path in config.paths {
+    //     for entry in WalkDir::new(path) {
+    //         match entry {
+    //             Err(e) => eprintln!("{}", e),
+    //             Ok(entry) => {
+    //                 if (config.entry_types.is_empty() || config.entry_types.iter().any(|t| {
+    //                     match t {
+    //                         Link => entry.file_type().is_symlink(),
+    //                         Dir => entry.file_type().is_dir(),
+    //                         File => entry.file_type().is_file(),
+    //                     }
+    //                 })) && (config.names.is_empty() || config.names.iter().any(|re| {
+    //                     re.is_match(&entry.file_name().to_string_lossy())
+    //                 }))
+    //                 {
+    //                     println!("{}", entry.path().display());
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
     Ok(())
 }
